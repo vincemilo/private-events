0cf45e8020563c9d94cd654b5b3f91b9a2c336b7
vincemilo <vincemilo@gmail.com>
2022-05-24 15:15:24 -0600

    added up to a button in Events#show adding attendees but currently not working

--- app/controllers/attended_event_controller.rb
+++ app/controllers/attended_event_controller.rb
@@ -1,7 +1,13 @@
 class AttendedEventController < ApplicationController
 
   def create
-    @attended_event = AttendedEvent.new(attended_event_params)
-  end
+    @current_event = Event.find(params[:event_id])
+#     @attended_event = current_user.attended_event.build(event_id: current_event.id)
 
+#     if @attended_event.save
+#       redirect_to @event
+#     else
+#       render :new, status: :unprocessable_entity
+#     end
+  end
 end
