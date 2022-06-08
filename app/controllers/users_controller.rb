8853199a4c5ddbda2dc9d72d6a465bb351ce24c9
vincemilo <vincemilo@gmail.com>
2022-06-07 22:06:44 -0600

    user show page now has attended and upcoming events separated by date query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @hosted_events = @user.events
+    @attended_events = @user.attended_events
   end
 end
